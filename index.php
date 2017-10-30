4fe0d5ffc14252ca32b682ab9b3c67040ebe691a
Eric Brian <eric.brianb12@gmail.com>
2017-10-30 11:46:17 -0200

    subindo parte html

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Odontologia Sanches</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="descrition" content="Descrição do meu website">
+	<meta name="keywords" content="palavras-chave,do,meu,website">
+	<meta charset="utf-8" />
+</head>
+<body>
+
+	<header>
+		<div class="center">
+			<div class="logo"> Logo marca</div><!-- logo -->
+			<nav class="desktop">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+			<nav class="mobile">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+		</div><!-- center -->
+	</header>
+
+	<section class="banner-principal">
+		<div class="center">
+			<form>
+				<h2>Qual o seu melhor E-mail?</h2>
+				<input type="email" name="email" required>
+				<input type="submit" name="acao" value="Cadastrar!">
+			</form>
+		</div><!-- center -->
+	</section><!-- banner principal -->
+
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="w50">
+				<h2> Jessica Sanches</h2>
+				<p>
+					Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+				</p>
+				<p>
+					Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+				</p>
+			</div><!-- w50 -->
+
+			<div class="w50">
+				<img src="images/foto.png">
+			</div> <!-- w50 -->
+
+		</div><!-- center -->
+	</section><!-- descricao autor -->
+
+	<section class="especialidades">
+		<div class="center">
+
+			<h2 class="title">Esepecialidades</h2>
+
+			<div class="box-especialidade">
+				<h3>Icone 1</h3>
+				<h3>CSS 3</h3>
+				<p>
+					Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+				</p>
+			</div><!-- box-especialidade -->
+
+			<div class="box-especialidade">
+				<h3>Icone 2</h3>
+				<h3>HTML5</h3>
+				<p>
+					Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+				</p>
+
+			</div><!-- box-especialidade -->
+
+			<div class="box-especialidade">
+				<h3>Icone 3</h3>
+				<h3>JavaScript</h3>
+				<p>
+					Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+				</p>
+
+			</div><!-- box-especialidade -->
+
+		</div>
+	</section><!-- especialidades -->
+
+	<section class="extras">
+		
+		<div class="center">
+			<div class="w50">
+				<h2 class="title">Depoimentos</h2>
+				<div class="depoimento-single">
+					<p>
+						Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+					</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+
+				</div><!-- depoimento-single -->
+
+				<div class="depoimento-single">
+					<p>
+						Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+					</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+					
+				</div><!-- depoimento-single -->
+
+					<div class="depoimento-single">
+					<p>
+						Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+					</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+					
+				</div><!-- depoimento-single -->
+
+			</div><!-- w50 -->	
+
+			<div class="w50">
+				<h2 class="title">Serviços</h2>
+				
+				<div class="servicos">
+					
+
+				<ul>
+					<li>Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+					</li>
+
+					<li>Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+					</li>
+
+					<li>Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Aenean aliquam molestie leo, vitae iaculis nisl. 
+					</li>
+				</ul>	
+
+				</div><!-- servicos -->
+			</div><!-- w50 -->	
+		</div><!-- center -->
+	</section><!-- extras -->
+
+	<footer>
+		<div class="center">
+			<p>Todos os direitos reservados</p>
+		</div><!-- center -->
+	</footer>
+
+
+</body>
+</html>
